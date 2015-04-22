212bd936c8839150ddbb8e07875cc80034f7416c
Glen Keane <glenkeane.94@gmail.com>
2015-04-22 11:28:33 +0000

    better parsing of output from lttng view

--- traceToObjectStream.js
+++ traceToObjectStream.js
@@ -8,43 +8,56 @@ function makeStream(){
     if (this._lastLineData){
       data = this._lastLineData + data;
     }
+
     var lines = data.split('\n');
-    this._lastLineData = lines.splice(lines.length-1,1)[0];
-    lines.forEach(function(line){
-      var trace;
-      trace = line.split('[')[1];
-      trace = trace.split('] (');
-      trace = [trace[0], trace[1].split(': {')[0], trace[1].split(': {')[1]];
-      trace = [trace[0], trace[1].split(') ')[0], trace[1].split(' ')[1], trace[1].split(' ')[2], trace[2]];
-      trace = [trace[0], trace[1], trace[2], trace[3], trace[4].split(' }, { ')[0], trace[4].split(' }, { ')[1]];
-      trace = [trace[0], trace[1], trace[2], trace[3], trace[4], trace[5].split(', ')];
-      trace[5][trace[5].length-1] = trace[5][trace[5].length-1].split(' }')[0];
-      trace = {
-        'time': trace[0],
-        'timeFromLast': trace[1],
-        'host': trace[2],
-        'name': trace[3],
-        'cpuId': trace[4].split(' = ')[1],
-        'eventData': trace[5]
-      };
-      var dd = {};
-      trace.eventData.forEach(function(d){
-        d.replace(/ /g, '');
-        dd[d.split(' = ')[0]] = d.split(' = ')[1];
-      });
-      trace.eventData = dd;
-      this.push(trace);
-    }.bind(this));
+
+    if(lines[lines.length-1] === ''){
+      lines.splice(lines.length-1,1);
+      this._lastLineData = null;
+    } else {
+      this._lastLineData = lines.splice(lines.length-1,1)[0];
+    }
+    
+    lines.forEach(parseLine.bind(this));
     done();
   };
   
   theStream._flush = function (done) {
     if (this._lastLineData){ 
-      this.push(this._lastLineData);
+      parseLine.call(this, this._lastLineData);
     }
     this._lastLineData = null;
     done();
   };
+
+  function parseLine (line){
+    var trace = {};
+
+    trace.time = line.match(/(?!\[).+?(?=\])/)[0];
+    trace.timeFromLast = line.match(/(?!.*?\().+?(?=\))/)[0];
+    
+    var hostAndTracepoint = line.match(/(?!.*?\))\b.*?(?=: {)/)[0];
+    trace.host = hostAndTracepoint.split(' ')[0];
+    trace.tracepoint = hostAndTracepoint.split(' ')[1];
+    trace.extInfo = line.match(/{(.*?)}/g);
+    for(var i = trace.extInfo.length - 1; i >= 0; i--){
+      trace.extInfo[i] = trace.extInfo[i].replace(/ =/g, ':');
+      trace.extInfo[i] = trace.extInfo[i].replace(/=/g, ':');
+      var matches = trace.extInfo[i].match(/[a-zA-Z\_\.]+(?=:)/g);
+      matches.forEach(function(elem){
+        trace.extInfo[i] = trace.extInfo[i].replace(elem, '"' + elem + '"');
+      });
+
+      trace.extInfo[i] = JSON.parse(trace.extInfo[i]);
+      var keys = Object.keys(trace.extInfo[i])
+      if(keys.length === 1){
+        trace[keys[0]] = trace.extInfo[i][keys[0]];
+        trace.extInfo.splice(i, 1);
+      }
+    };
+
+    this.push(trace);
+  }
 
   return theStream;
 }
